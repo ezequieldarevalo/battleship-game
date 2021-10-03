d04b0c9c4d1d53e2d8458bb3cfd96c35fd450996
Ezequiel Arevalo <ezequiel.d.arevalo@gmail.com>
2021-10-03 13:24:14 -0300

    carrier component transformed into generic ship component

---
+++ src/components/Ship/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+import Draggable from 'react-draggable';
+
+const GAMEBOARD_WIDTH = 400;
+const BOARD_SIZE = 10;
+
+interface ShipShapeProps {
+  orientation: string;
+  size: number;
+}
+
+export const ShipShape = styled.div`
+  background: grey;
+  float: left;
+  width: ${({ orientation, size }: ShipShapeProps) => (orientation === 'vertical' ? (GAMEBOARD_WIDTH / size) : ((GAMEBOARD_WIDTH / BOARD_SIZE) * size))}px;
+  height: ${({ orientation, size }: ShipShapeProps) => (orientation === 'horizontal' ? (GAMEBOARD_WIDTH / size) : ((GAMEBOARD_WIDTH / BOARD_SIZE) * size))}px;
+`;
+
+type SHIP_ORIENTATION='vertical' | 'horizontal'
+
+type SHIP_SIZE = 2 | 3 | 4;
+
+interface ShipProps {
+  orientation: SHIP_ORIENTATION;
+  size: SHIP_SIZE;
+}
+
+function Ship({ orientation, size }:ShipProps) {
+  return (
+    <Draggable cancel=".btn">
+      <ShipShape orientation={orientation} size={size} />
+    </Draggable>
+  );
+}
+
+export default Ship;
