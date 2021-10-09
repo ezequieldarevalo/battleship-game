2cc95e692b2d25c9ccc416c926dd81b334e6e1c4
Ezequiel Arevalo <ezequiel.d.arevalo@gmail.com>
2021-10-08 23:11:55 -0300

    straight lines validation

--- src/features/initialStage/InitialStage.tsx
+++ src/features/initialStage/InitialStage.tsx
@@ -18,11 +18,30 @@ const BOARD_SIZE = 10;
 const MOVEMENT_SIZE = 10;
 const CELL_SIZE_DESKTOP = GAMEBOARD_WIDTH_DESKTOP / BOARD_SIZE;
 const CELL_SIZE_MOBILE = GAMEBOARD_WIDTH_MOBILE / BOARD_SIZE;
+const CARRIER_ID = '1';
+const CRUISER1_ID = '2';
+const CRUISER2_ID = '3';
+const CRUISER3_ID = '4';
+const SUBMARINE_ID = '5';
 
 interface ShipShapeProps {
   vertical: boolean;
   size: number;
 }
+
+function getMovementCoordinatesFromCss(value: string): number[] {
+  const regexp = /(\d+)px, (\d+)/g;
+  return [+[...value.matchAll(regexp)][0][1],
+    +[...value.matchAll(regexp)][0][2]];
+}
+
+const isMultiple = (value: number, reference: number): boolean => {
+  const remainder = value % reference;
+  if (remainder === 0) {
+    return true;
+  }
+  return false;
+};
 
 export const ShipShape = styled.div`
   position: absolute;
@@ -35,26 +54,10 @@ export const ShipShape = styled.div`
   }
   `;
 
-// const GAMEBOARD_BORDER = 1;
-
-const getGameboardWidthFromDOM = ():number => {
-  const gameboard = document.getElementById('gb');
-  return (gameboard?.offsetWidth || 2) - (2 * GAMEBOARD_BORDER);
-};
-
-const getInitialCoordinate = (id: number):number => {
-  const gameboardWidth = getGameboardWidthFromDOM();
-  if (gameboardWidth === GAMEBOARD_WIDTH_DESKTOP) {
-    return (CELL_SIZE_DESKTOP) * (id - 1) + ((id - 1) * CELL_SIZE_DESKTOP);
-  }
-
-  return (CELL_SIZE_MOBILE) * (id - 1) + ((id - 1) * CELL_SIZE_MOBILE);
-};
-
 export type SHIP_SIZE = 2 | 3 | 4;
 
 export interface IInfoShip {
-  id: number;
+  id: string;
   size: SHIP_SIZE;
   vertical: boolean;
   initialX: number;
@@ -62,11 +65,24 @@ export interface IInfoShip {
   initialized: boolean;
 }
 
+interface IError {
+  has: boolean;
+  description: string;
+}
+
+const noError:IError = {
+  has: false,
+  description: '',
+};
+
 function InitialStage() {
+  const [error, setError] = useState<IError>(noError);
+  const divRef = React.useRef<HTMLDivElement>(null);
+  const carrier1Ref = React.useRef<HTMLDivElement>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [currentShipInfo, setCurrentShipInfo] = useState<IInfoShip | null>(null);
   const [carrier, setCarrier] = useState<IInfoShip>({
-    id: 1,
+    id: CARRIER_ID,
     size: 4,
     vertical: true,
     initialX: 0,
@@ -74,7 +90,7 @@ function InitialStage() {
     initialized: false,
   });
   const [cruiser1, setCruiser1] = useState<IInfoShip>({
-    id: 2,
+    id: CRUISER1_ID,
     size: 3,
     vertical: true,
     initialX: 0,
@@ -82,7 +98,7 @@ function InitialStage() {
     initialized: false,
   });
   const [cruiser2, setCruiser2] = useState<IInfoShip>({
-    id: 3,
+    id: CRUISER2_ID,
     size: 3,
     vertical: true,
     initialX: 0,
@@ -90,7 +106,7 @@ function InitialStage() {
     initialized: false,
   });
   const [cruiser3, setCruiser3] = useState<IInfoShip>({
-    id: 4,
+    id: CRUISER3_ID,
     size: 3,
     vertical: true,
     initialX: 0,
@@ -98,7 +114,7 @@ function InitialStage() {
     initialized: false,
   });
   const [submarine, setSubmarine] = useState<IInfoShip>({
-    id: 5,
+    id: SUBMARINE_ID,
     size: 2,
     vertical: true,
     initialX: 0,
@@ -106,34 +122,44 @@ function InitialStage() {
     initialized: false,
   });
 
-  const handleChangeOrientationById = (id: number, e:any): void => {
+  const getGameboardWidthFromDOM = (): number => divRef.current?.offsetWidth || 0;
+
+  const getInitialCoordinate = (id: number): number => {
+    const gameboardWidth = getGameboardWidthFromDOM() - (2 * GAMEBOARD_BORDER);
+    if (gameboardWidth === GAMEBOARD_WIDTH_DESKTOP) {
+      return (CELL_SIZE_DESKTOP) * (id - 1) + ((id - 1) * CELL_SIZE_DESKTOP);
+    }
+    return (CELL_SIZE_MOBILE) * (id - 1) + ((id - 1) * CELL_SIZE_MOBILE);
+  };
+
+  const handleChangeOrientationById = (id: string, e:any): void => {
     e.preventDefault();
     switch (id) {
-      case 1:
+      case CARRIER_ID:
         setCarrier((prev: IInfoShip) => ({
           ...prev,
           vertical: !prev.vertical,
         }));
         break;
-      case 2:
+      case CRUISER1_ID:
         setCruiser1((prev: IInfoShip) => ({
           ...prev,
           vertical: !prev.vertical,
         }));
         break;
-      case 3:
+      case CRUISER2_ID:
         setCruiser2((prev: IInfoShip) => ({
           ...prev,
           vertical: !prev.vertical,
         }));
         break;
-      case 4:
+      case CRUISER3_ID:
         setCruiser3((prev: IInfoShip) => ({
           ...prev,
           vertical: !prev.vertical,
         }));
         break;
-      case 5:
+      case SUBMARINE_ID:
         setSubmarine((prev: IInfoShip) => ({
           ...prev,
           vertical: !prev.vertical,
@@ -141,6 +167,15 @@ function InitialStage() {
         break;
       default: break;
     }
+    setCurrentShipInfo((prev: IInfoShip | null) => {
+      if (prev) {
+        return {
+          ...prev,
+          vertical: !prev.vertical,
+        };
+      }
+      return null;
+    });
   };
 
   useEffect(() => {
@@ -169,93 +204,99 @@ function InitialStage() {
     }, 3000);
   }, []);
 
-  // const shipsInBound = () => {
-  //   const gameboardWidth = getGameboardWidthFromDOM();
-  //   carrierInBounds =
-  // };
+  const shipsStraightLinesValidation = (cellSize: number) => {
+    const carrierTransformStyle: string = document.getElementById(CARRIER_ID)?.getAttribute('style') || '';
+    const cruiser1TransformStyle: string = document.getElementById(CRUISER1_ID)?.getAttribute('style') || '';
+    const cruiser2TransformStyle: string = document.getElementById(CRUISER2_ID)?.getAttribute('style') || '';
+    const cruiser3TransformStyle: string = document.getElementById(CRUISER3_ID)?.getAttribute('style') || '';
+    const submarineTransformStyle: string = document.getElementById(SUBMARINE_ID)?.getAttribute('style') || '';
+    let valid: boolean = true;
+    const allTranslations:number[] = [
+      ...getMovementCoordinatesFromCss(carrierTransformStyle),
+      ...getMovementCoordinatesFromCss(cruiser1TransformStyle),
+      ...getMovementCoordinatesFromCss(cruiser2TransformStyle),
+      ...getMovementCoordinatesFromCss(cruiser3TransformStyle),
+      ...getMovementCoordinatesFromCss(submarineTransformStyle)];
+    allTranslations.map((translation) => {
+      if (!isMultiple(translation, cellSize)) {
+        valid = false;
+      }
+      return 0;
+    });
+    return valid;
+  };
 
   const sendShips = () => {
-
+    const CELL_SIZE = (getGameboardWidthFromDOM() - (2 * GAMEBOARD_BORDER)) / BOARD_SIZE;
     // ships in bounds validation
-
-    // const gameboard = document.getElementById('gb');
-    // const gameboardWidth = gameboard?.offsetWidth || 0;
-    // console.log(gameboardWidth - (2 * GAMEBOARD_BORDER));
-
-    // const cruisers = Array.from(document.getElementsByClassName('cruiser'));
-    // console.log(cruisers);
-    // cruisers.map((cruiser) => {
-    //   console.log(cruiser.getAttribute('style'));
-    //   console.log(cruiser.getAttribute('id'));
-
-    //   console.log(cruiser.getAttribute('size'));
-    //   return '';
-    // });
+    if (!shipsStraightLinesValidation(CELL_SIZE)) { setError({ has: true, description: 'StraightLinesFailed' }); }
   };
 
   return (
     <Screen>
       <LoaderOverlay loading={loading}>
-        <Gameboard id="gb" type="player" ownShipsList={[]} destroyedShipsList={[]} hittedShipsList={[]} missedShipsList={[]}>
-          <>
-
-            {carrier.initialized && (
-            <Draggable onStart={() => setCurrentShipInfo(carrier)} bounds="parent" defaultPosition={{ x: carrier.initialX, y: carrier.initialY }} grid={[MOVEMENT_SIZE, MOVEMENT_SIZE]} cancel=".btn">
-              <ShipShape
-                className="carrier"
-                onContextMenu={(e: any) => handleChangeOrientationById(carrier.id, e)}
-                vertical={carrier.vertical}
-                size={carrier.size}
-                id={carrier.id.toString()}
-              />
-            </Draggable>
-            )}
-            {cruiser1.initialized && (
-            <Draggable onStart={() => setCurrentShipInfo(cruiser1)} bounds="parent" defaultPosition={{ x: cruiser1.initialX, y: cruiser1.initialY }} grid={[MOVEMENT_SIZE, MOVEMENT_SIZE]} cancel=".btn">
-              <ShipShape
-                className="cruiser"
-                onContextMenu={(e: any) => handleChangeOrientationById(cruiser1.id, e)}
-                vertical={cruiser1.vertical}
-                size={cruiser1.size}
-                id={cruiser1.id.toString()}
-              />
-            </Draggable>
-            )}
-            {cruiser2.initialized && (
-            <Draggable onStart={() => setCurrentShipInfo(cruiser2)} bounds="parent" defaultPosition={{ x: cruiser2.initialX, y: cruiser2.initialY }} grid={[MOVEMENT_SIZE, MOVEMENT_SIZE]} cancel=".btn">
-              <ShipShape
-                className="cruiser"
-                onContextMenu={(e: any) => handleChangeOrientationById(cruiser2.id, e)}
-                vertical={cruiser2.vertical}
-                size={cruiser2.size}
-                id={cruiser2.id.toString()}
-              />
-            </Draggable>
-            )}
-            {cruiser3.initialized && (
-            <Draggable onStart={() => setCurrentShipInfo(cruiser3)} bounds="parent" defaultPosition={{ x: cruiser3.initialX, y: cruiser3.initialY }} grid={[MOVEMENT_SIZE, MOVEMENT_SIZE]} cancel=".btn">
-              <ShipShape
-                className="cruiser"
-                onContextMenu={(e: any) => handleChangeOrientationById(cruiser3.id, e)}
-                vertical={cruiser3.vertical}
-                size={cruiser3.size}
-                id={cruiser3.id.toString()}
-              />
-            </Draggable>
-            )}
-            {submarine.initialized && (
-            <Draggable onStart={() => setCurrentShipInfo(submarine)} bounds="parent" defaultPosition={{ x: submarine.initialX, y: submarine.initialY }} grid={[MOVEMENT_SIZE, MOVEMENT_SIZE]} cancel=".btn">
-              <ShipShape
-                className="submarine"
-                onContextMenu={(e: any) => handleChangeOrientationById(submarine.id, e)}
-                vertical={submarine.vertical}
-                size={submarine.size}
-                id={submarine.id.toString()}
-              />
-            </Draggable>
-            )}
-          </>
-        </Gameboard>
+        <div ref={divRef}>
+          <Gameboard id="gb" type="player" ownShipsList={[]} destroyedShipsList={[]} hittedShipsList={[]} missedShipsList={[]}>
+            <>
+              {carrier.initialized && (
+              <Draggable onStart={() => setCurrentShipInfo(carrier)} bounds="parent" defaultPosition={{ x: carrier.initialX, y: carrier.initialY }} grid={[MOVEMENT_SIZE, MOVEMENT_SIZE]} cancel=".btn">
+                <ShipShape
+                  ref={carrier1Ref}
+                  className="carrier"
+                  onContextMenu={(e: any) => handleChangeOrientationById(carrier.id, e)}
+                  vertical={carrier.vertical}
+                  size={carrier.size}
+                  id={carrier.id}
+                />
+              </Draggable>
+              )}
+              {cruiser1.initialized && (
+              <Draggable onStart={() => setCurrentShipInfo(cruiser1)} bounds="parent" defaultPosition={{ x: cruiser1.initialX, y: cruiser1.initialY }} grid={[MOVEMENT_SIZE, MOVEMENT_SIZE]} cancel=".btn">
+                <ShipShape
+                  className="cruiser"
+                  onContextMenu={(e: any) => handleChangeOrientationById(cruiser1.id, e)}
+                  vertical={cruiser1.vertical}
+                  size={cruiser1.size}
+                  id={cruiser1.id}
+                />
+              </Draggable>
+              )}
+              {cruiser2.initialized && (
+              <Draggable onStart={() => setCurrentShipInfo(cruiser2)} bounds="parent" defaultPosition={{ x: cruiser2.initialX, y: cruiser2.initialY }} grid={[MOVEMENT_SIZE, MOVEMENT_SIZE]} cancel=".btn">
+                <ShipShape
+                  className="cruiser"
+                  onContextMenu={(e: any) => handleChangeOrientationById(cruiser2.id, e)}
+                  vertical={cruiser2.vertical}
+                  size={cruiser2.size}
+                  id={cruiser2.id}
+                />
+              </Draggable>
+              )}
+              {cruiser3.initialized && (
+              <Draggable onStart={() => setCurrentShipInfo(cruiser3)} bounds="parent" defaultPosition={{ x: cruiser3.initialX, y: cruiser3.initialY }} grid={[MOVEMENT_SIZE, MOVEMENT_SIZE]} cancel=".btn">
+                <ShipShape
+                  className="cruiser"
+                  onContextMenu={(e: any) => handleChangeOrientationById(cruiser3.id, e)}
+                  vertical={cruiser3.vertical}
+                  size={cruiser3.size}
+                  id={cruiser3.id}
+                />
+              </Draggable>
+              )}
+              {submarine.initialized && (
+              <Draggable onStart={() => setCurrentShipInfo(submarine)} bounds="parent" defaultPosition={{ x: submarine.initialX, y: submarine.initialY }} grid={[MOVEMENT_SIZE, MOVEMENT_SIZE]} cancel=".btn">
+                <ShipShape
+                  className="submarine"
+                  onContextMenu={(e: any) => handleChangeOrientationById(submarine.id, e)}
+                  vertical={submarine.vertical}
+                  size={submarine.size}
+                  id={submarine.id}
+                />
+              </Draggable>
+              )}
+            </>
+          </Gameboard>
+        </div>
       </LoaderOverlay>
       <MobilePanel>
         {currentShipInfo && (
@@ -274,6 +315,7 @@ function InitialStage() {
         )}
       </MobilePanel>
       <div>
+        {error.has && <div>{error.description }</div>}
         <Button onClick={sendShips}>
           START GAME
         </Button>
