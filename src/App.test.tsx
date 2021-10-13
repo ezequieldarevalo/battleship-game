0e41057e3943dd1314e69b9d81120b9917bf1483
Ezequiel Arevalo <ezequiel.d.arevalo@gmail.com>
2021-10-13 18:24:49 -0300

    I18n - testing language

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,14 +3,35 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import publicInitialMessages from './components/common/messages/es-AR.json';
+import spanishInitialMessages from './components/common/messages/es-AR.json';
+import englishInitialMessages from './components/common/messages/en-US.json';
 
-test('renders learn react link', () => {
+test('renders component respecting spanish language', () => {
   const { getByText } = render(
     <Provider store={store}>
-      <App initialLang="es-AR" initialMessages={publicInitialMessages} />
+      <App initialLang="es-AR" initialMessages={spanishInitialMessages} />
     </Provider>,
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText(/BATALLA NAVAL/i)).toBeInTheDocument();
+});
+
+test('renders component respecting english language', () => {
+  const { getByText } = render(
+    <Provider store={store}>
+      <App initialLang="en-US" initialMessages={englishInitialMessages} />
+    </Provider>,
+  );
+
+  expect(getByText(/BATTLESHIP GAME/i)).toBeInTheDocument();
+});
+
+test('renders initial stage', () => {
+  const { getByText } = render(
+    <Provider store={store}>
+      <App initialLang="en-US" initialMessages={englishInitialMessages} />
+    </Provider>,
+  );
+
+  expect(getByText(/START GAME/i)).toBeInTheDocument();
 });
