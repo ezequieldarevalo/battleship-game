291f0f731a13b3ed5df83fadbffd229679343b65
Ezequiel Arevalo <ezequiel.d.arevalo@gmail.com>
2021-10-14 23:46:10 -0300

    not showing own ship cells in cpu gameboard test

--- src/features/battleship/Gameboard.test.tsx
+++ src/features/battleship/Gameboard.test.tsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import Gameboard from './Gameboard';
 import { initialGameboardState } from '../../lib/common/constants';
 import { store } from '../../app/store';
+import { gamboardWithFirstOwnCell } from '../../lib/common/testConstants';
 
 test('renders component', () => {
   const component = render(
@@ -25,7 +26,7 @@ test('renders component without title name', () => {
   expect(component.container).not.toHaveTextContent('example');
 });
 
-test('renders component without title name', () => {
+test('renders component with title name', () => {
   const component = render(
     <Provider store={store}>
       <Gameboard id="example" withName type="human" gameState={initialGameboardState} />
@@ -33,4 +34,46 @@ test('renders component without title name', () => {
   );
 
   expect(component.container).toHaveTextContent('example');
+});
+
+test('renders component with no gameState every transparent backgrounds ', () => {
+  const component = render(
+    <Provider store={store}>
+      <Gameboard id="example" type="human" />
+    </Provider>,
+  );
+
+  expect(component.container.querySelector('div[type="human"]')).toHaveStyle('background: transparent');
+});
+
+test('renders component with no gameState shows full board', () => {
+  const component = render(
+    <Provider store={store}>
+      <Gameboard id="example" type="human" />
+    </Provider>,
+  );
+
+  expect(component.container.querySelectorAll('div[type="human"]')).toHaveLength(100);
+});
+
+test('renders component with first own cell not showed in cpu gamboard', () => {
+  const component = render(
+    <Provider store={store}>
+      <Gameboard id="example" type="cpu" gameState={gamboardWithFirstOwnCell} />
+    </Provider>,
+  );
+
+  expect(component.container.querySelector('div[type="cpu"]'))
+    .toHaveStyle('background: transparent');
+});
+
+test('renders component with own cells showed in human gamboard', () => {
+  const component = render(
+    <Provider store={store}>
+      <Gameboard id="example" type="human" gameState={gamboardWithFirstOwnCell} />
+    </Provider>,
+  );
+
+  expect(component.container.querySelector('div[type="human"]'))
+    .toHaveStyle('background: grey');
 });
